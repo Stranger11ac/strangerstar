b2c288189744a322b8ebb501f91663ddf065659b
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2025-02-13 20:05:47 -0600

    Add initial Django project structure with jobs and classes apps

---
+++ classes/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def clasindex(request):
+    return render(request, 'other.html', {})
+
---
+++ jobs/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("jobs/", views.jobs, name="jobs"),
+]
