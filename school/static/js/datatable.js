ac39b225615a1a8dd6049c312eafaae50a94400e
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2025-03-07 01:17:08 -0600

    Update CSS for submenu behavior, add new icon button, and improve DataTable settings

--- school/static/js/datatable.js
+++ school/static/js/datatable.js
@@ -3,15 +3,19 @@ $(document).ready(function () {
         lengthChange: false,
         pageLength: 10, // Cantidad de registros por página
         order: [[0, "asc"]], // Ordenar por ID de forma descendente
+        columnDefs: [
+            { orderable: false, targets: 4 }, // Desactiva ordenación en la columna 4
+            { searchable: false, targets: [0, 4] }, // Desactiva búsqueda en las columnas
+        ],
         responsive: true, // Adaptar a pantallas pequeñas
         autoWidth: false, // Evitar que DataTables altere los anchos de columna
         language: {
             decimal: ",",
             thousands: ".",
-            search: "Buscar:",
-            info: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            search: "",
+            info: "_START_ - _END_ de _TOTAL_",
             infoEmpty: "",
-            infoFiltered: "(_MAX_ registros totales)",
+            infoFiltered: "(_MAX_ en totales)",
             zeroRecords: "No se encontraron coincidencias",
             paginate: {
                 first: "<span class='ic-solar double-alt-arrow-left'></span>",
@@ -22,6 +26,5 @@ $(document).ready(function () {
         },
     });
 
-    
     $("#dt-search-0").attr("placeholder", "Buscar:");
 });
