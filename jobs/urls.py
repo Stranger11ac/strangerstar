bdb3da59e32ce97a5e2998d8951dd5389422b901
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2025-03-11 19:45:35 -0600

    Update language in base.html, modify URL paths in urls.py, enhance CSS styles in introjs.min.css, and implement weather tutorial in intro.js and weather.html

--- jobs/urls.py
+++ jobs/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("singin/", views.singin, name="singin"),
     path("singout/", views.singout, name="singout"),
 
-    path("proyectos/app_clima/", views.weather_app, name="weather_app"),
-    path("proyectos/spin/", views.spin_app, name="spin_app"),
+    path("proyectos/weather_app/", views.weather_app, name="weather_app"),
+    path("proyectos/spin_app/", views.spin_app, name="spin_app"),
     path("proyectos/spin_up_list/", views.spin_up_list, name="spin_up_list"),
 ]
--- school/static/js/intro.js
+++ school/static/js/intro.js
@@ -37,10 +37,64 @@ if (!localStorage.getItem("viewSpinIntro")) {
     spinIntro();
 }
 
-document.getElementById("startTour").addEventListener("click", function () {
+$("#startSpinTour").on("click", function () {
     localStorage.removeItem("viewSpinIntro");
 
     setTimeout(() => {
         spinIntro();
+    }, 2000);
+});
+
+function weatherIntro() {
+    introJs()
+        .setOptions({
+            steps: [
+                {
+                    title: "Bienvenidos 👋😋",
+                    intro: "Esta app de clima es sencilla. Te recomiendo hacer este pequeño tutorial. <i class='ic-solar cloud-sun-bold-duotone'></i>",
+                },
+                {
+                    title: "Advertencia 😥⚠️🧐",
+                    intro: "Los datos de esta app no son presizos ya que son datos publicos y gratuitos de <a href='weatherapi.com' target='_blank' rel='noopener noreferrer'>weatherapi.com</a> <i class='ic-solar confounded-circle-bold-duotone'></i>",
+                },
+                {
+                    element: "#searchCoords",
+                    intro: "Puedes buscar el clima en tu ubicacion actual <i class='ic-solar gps-bold-duotone'></i>, al permitir el acceso a tu ubicacion los datos no son compartidos, solamente se utilizan para mostarte el clima de la ciudad actual.",
+                },
+                {
+                    element: "#locationGroup",
+                    intro: "Escribe en el campo para buscar el clima de una ciudad en concreto <i class='ic-solar map-point-search'></i>. Tu busqueda puede ser mas precisa al incluir el estado o provincia para ser mas especifico",
+                },
+                {
+                    element: "#btnLocation",
+                    intro: "Despues de escribir la localidad puedes presionar 'Enter' o hacer click en este boton. <i class='ic-solar minimalistic-magnifer'></i>",
+                },
+                {
+                    intro: "Los datos del clima se actualizan solamente con una nueva busqueda pero los datos anteriores persisten hasta que se elimine el cache. <i class='ic-solar settings'></i> <i class='ic-solar server-square-update'></i>",
+                },
+                {
+                    intro: "Listo!!! 🎉🎉🎉😋👋 <br/> Puedes repetir el tutorial en las configuraciones. <i class='ic-solar settings'></i>",
+                },
+            ],
+            showBullets: true,
+            nextLabel: '<i class="ic-solar alt-arrow-right"></i>',
+            prevLabel: '<i class="ic-solar alt-arrow-left"></i>',
+            doneLabel: '<i class="ic-solar check-circle-bold-duotone"></i>',
+        })
+        .onexit(function () {
+            localStorage.setItem("viewWeatherIntro", "true");
+        })
+        .start();
+}
+
+if (!localStorage.getItem("viewWeatherIntro")) {
+    weatherIntro();
+}
+
+$("#startWeatherTour").on("click", function () {
+    localStorage.removeItem("viewWeatherIntro");
+
+    setTimeout(() => {
+        weatherIntro();
     }, 2000);
 });
