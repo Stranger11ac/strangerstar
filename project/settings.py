2c969829714ed80ba0b4bed2653cabf95a434631
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2025-03-14 19:23:42 -0600

    Update STATIC_ROOT path in settings.py for correct static file handling

--- project/settings.py
+++ project/settings.py
@@ -79,7 +79,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',  # Carpeta global compartida
 # ]
