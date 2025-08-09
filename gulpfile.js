182c66cd1b1c0c02798b6023a453bd3239eb4368
Stranger11ac <salvahdzcep@gmail.com>
2025-08-08 21:45:40 -0600

    add dependences: gulp-sass. create function to watch changes in SASS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
-function hola() {
-    toast(center, 8000, info, "Hola desde GULP")
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import {src,dest, watch} from 'gulp';
+
+const sass = gulpSass(dartSass);
+export function css(done) {
+    src('static/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('static/css'));
+
+    done();
+}
+
+export function watchFiles() {
+    watch('static/scss/**/*.scss', css);
 }
